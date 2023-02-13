315066fa7149f41e4bbd2976ce1303e46244e4fc
Svetlana <start61@mail.ru>
2023-02-13 16:14:06 +0300

    Added Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -4,6 +4,17 @@
 //-3 -> нет
 //7 -> нет
 
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+count = N%2; //int 0, 1
+
+if (count == 0)
+Console.Write("Да");
+else
+Console.Write("Нет");
+
+    
 
 
 
